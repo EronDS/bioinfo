0da631598507d315809f3752ee2778879bfd26d4
Eron DS <erondatascience@gmail.com>
2023-08-02 08:03:57 +0000

    02/08

--- Course1/week3.py
+++ Course1/week3.py
@@ -1,4 +1,5 @@
 from itertools import product
+import copy
 
 class week3:
     def __init__(self):
@@ -74,20 +75,19 @@ class week3:
             prob *= matrix[nucleotide][i]
         return prob
     
-    def ProfileMostProbableKmer(self,dna:str, k:int, matrix:dict) -> str:
+    def ProfileMostProbableKmer(self, dna: str, k: int, matrix: dict) -> str:
         kmers = self.GenerateKmers([dna], k)
+        
         prob = 0.0
         most_probable = ''
-
         for mer in kmers:
             mer_prob = self.Profile(mer, matrix)
             if mer_prob > prob:
                 prob = mer_prob
                 most_probable = mer
 
-        print(f'Most Probable K-mer: {most_probable}\n\
-         Probability: {prob}')
         return most_probable
+
     
     def PromptProfileMostProbableKmer(self,file_directory:str) -> str:
 
@@ -133,29 +133,73 @@ class week3:
                 map_freq[nucleotide][i] = map_[nucleotide][i] / total
 
         return map_freq
-
     
-    def GreedyMotifSearch(self,k:int,t:int,dna:list) -> list:
-        best_motifs = [] 
-        motifs = []
+    def Score(self,kmers:list):
+        k = len(kmers[0])
+        map_ = {'A': [0 for _ in range(k)],
+                'C': [0 for _ in range(k)],
+                'G': [0 for _ in range(k)],
+                'T': [0 for _ in range(k)]}
 
-        for string in dna:
-            kmers = self.GenerateKmers([string],k)
-            motif_score = self.GenerateProfileMatrix(kmers)
-            motif_i = self.ProfileMostProbableKmer(string,k,motif_score)
-            motifs.append(motif_i)
+        # count kmer in dictionary (for each position)
+        for kmer in kmers:
+            for i in range(len(kmer)):
+                nucleotide = kmer[i]
+                map_[nucleotide][i] += 1
+        
+        c = 0
+        most_frequents = [] 
 
-        return motifs
+        for i in range(k):
+            max_value = 0
+            for nucl in map_.keys():
+                if map_[nucl][i] > max_value:
+                    max_value = map_[nucl][i]
+                    most_frequent = nucl
+
+            most_frequents.append(most_frequent)
+
+        for mer in kmers:
+            for i in range(len(mer)):
+                if mer[i] != most_frequents[i]:
+                    c += 1
+        return c
+    
+
+    def GreedyMotifSearch(self, k: int, t: int, dna: list) -> list:
+        best_motifs = [string[:k] for string in dna]
+        motifs = [string[:k] for string in dna]
+        kmers = self.GenerateKmers([dna[0]], k)
+
+        for i in range(len(kmers)):
+            motifs[0] = kmers[i]
+            for u in range(1, t):
+                profile = self.GenerateProfileMatrix(motifs[:u])
+                most_probable_motif = self.ProfileMostProbableKmer(dna[u], k, profile)
+                if most_probable_motif!='': motifs[u] = most_probable_motif
+
+
+            if self.Score(motifs) < self.Score(best_motifs):
+                best_motifs = motifs.copy()  # Create a new list for best_motifs
+
+        return best_motifs
+
+
+
+
     
     def PromptGreedyMotifSearch(self,file_directory:str) -> str:
         with open(file_directory,'r') as f:
             lines = f.readlines()
             k,t = [int(i) for i in lines[0].rstrip().split()]
-            dna = lines[1].rstrip().split()
-    
+            dna = list(lines[1].rstrip().split())
+
         ans = self.GreedyMotifSearch(k,t,dna)
         self.ans = ' '.join(ans)
+        with open('/workspace/bioinfo/results.txt', 'w') as w:
+            w.write(self.ans)
         return self.ans
 
-prompt_file = '/workspace/bioinfo/Course1/prompt.txt'
+prompt_file = '/workspace/bioinfo/GreedyMotifSearch/inputs/input_1.txt'
 print(week3().PromptGreedyMotifSearch(prompt_file))
+
